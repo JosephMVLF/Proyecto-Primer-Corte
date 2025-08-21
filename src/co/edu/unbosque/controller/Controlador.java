220cef99eecba8b57a505f3ffca62a07921667a2
José Efraín Caicedo Delgado <jopevicdieke@gmail.com>
2025-08-21 01:26:54 -0500

    Adelante parte de la estructura del controlador para probar el programa
    desde consola

--- src/co/edu/unbosque/controller/Controlador.java
+++ src/co/edu/unbosque/controller/Controlador.java
@@ -1,17 +1,25 @@
 package co.edu.unbosque.controller;
+import java.lang.classfile.instruction.SwitchCase;
+
+import co.edu.unbosque.model.Libro;
 import co.edu.unbosque.model.TrabajoDeGrado;
+import co.edu.unbosque.model.persistence.LibroDAO;
 import co.edu.unbosque.model.persistence.TrabajoDeGradoDAO;
 import co.edu.unbosque.view.Consola;
 import co.edu.unbosque.view.MenuPrincipal;
+
 
 public class Controlador {
 
 	private Consola con;
 	private MenuPrincipal mp;
+	private LibroDAO obL;
+	
 
 	public Controlador() {
 		con = new Consola();
 		mp = new MenuPrincipal();
+		obL = new LibroDAO();
 	}
 
 	public void runGUI() {
@@ -21,17 +29,61 @@ public class Controlador {
 	
 
 	public void run() {
-		
-		 TrabajoDeGradoDAO dao = new TrabajoDeGradoDAO();
-
-	        dao.crear(new TrabajoDeGrado("Ana", "IA en Educación", 2023));
-	        dao.crear(new TrabajoDeGrado("Luis", "Robótica Avanzada", 2022));
-	        dao.crear(new TrabajoDeGrado("Ana", "Big Data", 2021));
-
-	        System.out.println("Buscar por autor:");
-	        dao.filtrar("Luis");
-
-	        System.out.println("\nBuscar por año:");
-	        dao.filtrar(2023);
+		int opcion=0;
+		do {
+			con.escribirSinSalto("Eliga la opción que desea realizar ");
+			con.escribirConSalto("1. Crear"
+					+ "\n2. Mostrar"
+					+ "\n3. Eliminar"
+					+ "\n4. Actualizar"
+					+ "\n5. Filtrar"
+					+ "\n6. Salir"
+					+ "");
+			opcion = con.leerEntero();
+			switch (opcion) {
+			case 1:
+				con.escribirConSalto("Titulo");
+				String titulo = con.leerLineaCompleta();
+				con.escribirConSalto("Autor");
+				String autor = con.leerLineaCompleta();
+				con.escribirConSalto("Anio");
+				int anio = con.leerEntero();
+				con.escribirConSalto("Genero");
+				String genero = con.leerLineaCompleta();
+				con.escribirConSalto("Pagina");
+				int pagina = con.leerEntero();
+				
+				Libro l1 = new Libro(titulo, autor, anio, genero, pagina);
+				obL.crear(l1);
+				
+				for (Libro libro : obL.getListaLibro()) {
+				libro.toString();	
+				}
+				
+				break;
+			case 2:
+				obL.mostrar();
+				break;
+			case 3:
+				/*con.escribirConSalto("Digite el indice o el nombre");
+				int indice = con.leerEntero();
+				Libro objetoAEliminar = obL.filtrar(con.leerPalabra());
+				obL.eliminiar(indice);
+				obL.eliminiar(l1);
+				*/
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				break;
+			case 6:
+				con.escribirConSalto("Hasta Luego");
+				break;
+			default:
+				con.escribirConSalto("Digitó una opción invalida. Porfavor Vuelva a intentar");
+				break;
+			}
+		} while (opcion!=6);
 	}
 }
