e682a527cb8bd6e6ae6727d6d957f3354d4d0bfe
TomasTriana <tomastrianag@gmail.com>
2025-08-22 01:21:24 -0500

    .

--- src/co/edu/unbosque/view/MostrarDatos.java
+++ src/co/edu/unbosque/view/MostrarDatos.java
@@ -22,7 +22,7 @@ public class MostrarDatos extends JFrame {
 
         setTitle("Resumen de Publicaciones");
         setSize(800, 600);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
         int totalGeneral = totalLibros + totalArticulos + totalPartituras + totalPeliculas +
@@ -68,3 +68,4 @@ public class MostrarDatos extends JFrame {
         return new JScrollPane(area);
     }
 }
+
